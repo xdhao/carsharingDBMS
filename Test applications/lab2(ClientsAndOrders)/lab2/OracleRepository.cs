02f9d14b5d5a214a790f04bf682aca083d13c915
Dmitriy <kapralov_00@mail.ru>
2021-04-14 19:26:16 +0300

    Исправлен код c using

--- Test applications/lab2(ClientsAndOrders)/lab2/OracleRepository.cs
+++ Test applications/lab2(ClientsAndOrders)/lab2/OracleRepository.cs
@@ -19,20 +19,18 @@ namespace lab2
         {
             try
             {
-                using (OracleConnection con = new OracleConnection(connectionString))
-                {
-                    con.Open();
-                    string query = "INSERT INTO reviews (text, raiting, vin, id_clients) VALUES (:text, :raiting, :vin, :id_clients)";
-                    OracleCommand command = new OracleCommand(query, con);
-                    OracleParameter[] parameters = new OracleParameter[] {
+                using var con = new OracleConnection(connectionString);
+                con.Open();
+                string query = "INSERT INTO reviews (text, raiting, vin, id_clients) VALUES (:text, :raiting, :vin, :id_clients)";
+                OracleCommand command = new OracleCommand(query, con);
+                OracleParameter[] parameters = new OracleParameter[] {
                     new OracleParameter("text", newreviews.text),
                     new OracleParameter("raiting", newreviews.raiting),
                     new OracleParameter("vin", newreviews.vin),
                     new OracleParameter("id_clients", newreviews.id_clients)
                     };
-                    command.Parameters.AddRange(parameters);
-                    command.ExecuteNonQuery();
-                }
+                command.Parameters.AddRange(parameters);
+                command.ExecuteNonQuery();
                 return true;
             }
             catch (Exception ex)
@@ -44,59 +42,46 @@ namespace lab2
         public IEnumerable<Reviews> GetReviews()
         {
             var result = new List<Reviews>();
-            using (OracleConnection con = new OracleConnection(connectionString))
+            using var con = new OracleConnection(connectionString);
+            con.Open();
+            string rev = "SELECT * FROM reviews";
+            using var command = new OracleCommand(rev, con);
+            using var readreRev = command.ExecuteReader();
+            while (readreRev.Read())
             {
-                con.Open();
-                string rev = "SELECT * FROM reviews";
-                using (OracleCommand command = new OracleCommand(rev, con))
-                {
-                    using (OracleDataReader readreRev = command.ExecuteReader())
-                    {
-                        while (readreRev.Read())
-                        {
-                            result.Add(new Reviews(readreRev.GetInt32(0), readreRev.GetString(1), readreRev.GetFloat(2), readreRev.GetString(3), readreRev.GetInt32(4)));
-                        }
-                    }
-                }
+                result.Add(new Reviews(readreRev.GetInt32(0), readreRev.GetString(1), readreRev.GetFloat(2), readreRev.GetString(3), readreRev.GetInt32(4)));
             }
             return result;
         }
         public void DeleteReviews(Reviews newrivews)
         {
-            using (OracleConnection con = new OracleConnection(connectionString))
-            {
-                con.Open();
-                string delete = "DELETE FROM reviews WHERE id = :id";
-                using (OracleCommand command = new OracleCommand(delete, con))
-                {
-                    OracleParameter parameter = new OracleParameter("id", newrivews.id);
-                    command.Parameters.Add(parameter);
-                    command.ExecuteNonQuery();
-                }
-            }
+            using var con = new OracleConnection(connectionString);
+            con.Open();
+            string delete = "DELETE FROM reviews WHERE id = :id";
+            using var command = new OracleCommand(delete, con);
+            OracleParameter parameter = new OracleParameter("id", newrivews.id);
+            command.Parameters.Add(parameter);
+            command.ExecuteNonQuery();
         }
         public bool UpdateReviews(Reviews newreviews)
         {
             try
             {
-                using (OracleConnection con = new OracleConnection(connectionString))
-                {
-                    con.Open();
-                    string update = "UPDATE reviews SET text = :text, raiting = :raiting, vin = :vin, id_clients = :id_clients WHERE id = :id";
-                    using (OracleCommand command = new OracleCommand(update, con))
-                    {
-                        OracleParameter[] parameters = new OracleParameter[] {
-                        new OracleParameter("text", newreviews.text),
-                        new OracleParameter("raiting", newreviews.raiting),
-                        new OracleParameter("vin", newreviews.vin),
-                        new OracleParameter("id_clients", newreviews.id_clients),
-                        new OracleParameter("id", newreviews.id)
-                        };
-                        command.Parameters.AddRange(parameters);
-                        command.ExecuteNonQuery();
-                    }
-                }
+                using var con = new OracleConnection(connectionString);
+                con.Open();
+                string update = "UPDATE reviews SET text = :text, raiting = :raiting, vin = :vin, id_clients = :id_clients WHERE id = :id";
+                using var command = new OracleCommand(update, con);
+                OracleParameter[] parameters = new OracleParameter[] {
+                    new OracleParameter("text", newreviews.text),
+                    new OracleParameter("raiting", newreviews.raiting),
+                    new OracleParameter("vin", newreviews.vin),
+                    new OracleParameter("id_clients", newreviews.id_clients),
+                    new OracleParameter("id", newreviews.id)
+            };
+                command.Parameters.AddRange(parameters);
+                command.ExecuteNonQuery();
                 return true;
+
             }
             catch (Exception ex)
             {
@@ -108,23 +93,17 @@ namespace lab2
 
         public string CalculationOfCost(Clients newClients)
         {
-            using (OracleConnection con = new OracleConnection(connectionString))
-            {
-                con.Open();
-                string calcucat = "select value*calculationcost(raiting) from clients " +
-                "join orders on clients.id = orders.id_clients " +
-                "join tarrifs on tarrifs.id = orders.id_tarrifs " +
-                "where clients.id = :id";
-                using (OracleCommand command = new OracleCommand(calcucat, con))
-                {
-                    OracleParameter parameter = new OracleParameter("id", newClients.id);
-                    command.Parameters.Add(parameter);
-                    object count = command.ExecuteScalar();
-                    return count.ToString();
-                }
-
-            }
-
+            using var con = new OracleConnection(connectionString);
+            con.Open();
+            string calcucat = "select value*calculationcost(raiting) from clients " +
+            "join orders on clients.id = orders.id_clients " +
+            "join tarrifs on tarrifs.id = orders.id_tarrifs " +
+            "where clients.id = :id";
+            using var command = new OracleCommand(calcucat, con);
+            OracleParameter parameter = new OracleParameter("id", newClients.id);
+            command.Parameters.Add(parameter);
+            object count = command.ExecuteScalar();
+            return count.ToString();
         }
     }
 }
--- Test applications/lab2(ClientsAndOrders)/lab2/Program.cs
+++ Test applications/lab2(ClientsAndOrders)/lab2/Program.cs
@@ -11,11 +11,11 @@ namespace lab2
             Irepository myRep = new OracleRepository();
             string obj = myRep.CalculationOfCost(new Clients(4, "", "", "", "", "", "", "", 0, 0));
             Console.WriteLine(obj);
-            //myRep.AddReviews(new reviews(1, "привет3", 2.5f, "awda241", 4));
-            //myRep.UpdateReviews(new Reviews(24, "66 баллов", 5.0f, "QEST21521", 6));
-            //myRep.DeleteReviews(new reviews(26, "", 0, "", 0));
-            /*var reviews = myRep.GetReviews();
-            foreach(var review in reviews)
+            //myRep.AddReviews(new Reviews(1, "привет4", 2.5f, "awda241", 4));
+            //myRep.UpdateReviews(new Reviews(24, "65 баллов", 5.0f, "QEST21521", 6));
+            //myRep.DeleteReviews(new Reviews(41, "", 0, "", 0));
+            var reviews = myRep.GetReviews();
+            /*foreach(var review in reviews)
             {
                 Console.WriteLine(review);
             }*/
